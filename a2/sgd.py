8a6cf6b591a9649fb330c7bd00ae24972d5f8b0a
EllaDing <yd2419@columbia.edu>
2020-04-15 22:38:09 -0700

    finished codiing 2.b

--- a2/sgd.py
+++ a2/sgd.py
@@ -87,7 +87,7 @@ def sgd(f, x0, step, iterations, postprocessing=None, useSaved=False,
         loss = None
         ### YOUR CODE HERE
         loss = -np.log(f(x))[0]
-        postprocessing = lambda x: x - step *f(x)[1]
+        postprocessing = lambda x: x - step * f(x)[1]
         ### END YOUR CODE
 
         x = postprocessing(x)
